a387a58d5e95acd21908f954c6e31e8af1a1530b
vbsreddy <vintabharathsaireddy@gmail.com>
2019-11-30 22:09:36 +0530

    GussingGameChallengeCompleted

--- Challenges/GuessingGameChallenge/GuessingGameChallenge.py
+++ Challenges/GuessingGameChallenge/GuessingGameChallenge.py
@@ -4,26 +4,32 @@ __date__ = '30/11/2019'
 from random import randint
 randomInt = randint(1,100)
 count = 0
-previousGuess = []
+previousGuess = [0]
+print("WELCOME TO GUESS ME!")
+print("I'm thinking of a number between 1 and 100")
+print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
+print("If your guess is within 10 of my number, I'll tell you you're WARM")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
 while(True):
-    a = int(input("Guess the number:"))
-    if a<1 or a>100:
+    guess = int(input("Guess the number:"))
+    if guess < 1 or guess > 100:
         print('OUT OF BOUNDS')
         continue
-    elif a== randomInt:
+    elif guess == randomInt:
         print("Congrats! You have guessed the number correctly.")
-        print(f'You have taken {count} guesses.')
+        print(f'You have taken {len(previousGuess)} guesse(s).')
         break
-    elif count==0:
-        if abs(a-randomInt) <= 10:
+    elif len(previousGuess) < 2:
+        if abs(guess-randomInt) <= 10:
             print('WARM!')
         else:
             print('COLD!')
     else:
         # if the previous guessed number is farther away from random-integer than current guessed number, if-block will execute.
-        if abs(previousGuess[-1]-randomInt) >= abs(a-randomInt):
+        if abs(previousGuess[-1]-randomInt) >= abs(guess-randomInt):
             print('WARMER!')
         else:
-            print('Farther!')
-    count +=1
-    previousGuess.append(a)
+            print('COLDER!')
+    previousGuess.append(guess)
