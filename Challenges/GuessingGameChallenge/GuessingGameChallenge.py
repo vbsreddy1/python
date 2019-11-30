312a23163fe6841e540f5887f6f61bf47bfe9175
vbsreddy <vintabharathsaireddy@gmail.com>
2019-11-30 21:21:09 +0530

    GuessingGame

--- Challenges/GuessingGameChallenge/GuessingGameChallenge.py
+++ Challenges/GuessingGameChallenge/GuessingGameChallenge.py
@@ -1,3 +1,29 @@
 __author__ = 'VBSREDDY'
 __date__ = '30/11/2019'
 
+from random import randint
+randomInt = randint(1,100)
+print(randomInt)
+count = 0
+previousGuess = []
+while(True):
+    a = int(input("Guess the number:"))
+    previousGuess.append(a)
+    print(previousGuess)
+    
+    if a== randomInt:
+        print("Congrats! You have guessed the number correctly.:-)")
+        print(f'You have taken {count} guesses.')
+        break
+    elif count==0:
+        if abs(a-randomInt) <= 10:
+            print('WARM!')
+        else:
+            print('COLD!')
+    else:
+        if abs(previousGuess[-1]-randomInt) <= abs(a-randomInt) and abs(previousGuess[-1]-randomInt)<=10:
+            print('WARMER!')
+        else:
+            print('Farther!')
+    count +=1
+
--- Challenges/GuessingGameChallenge/README.md
+++ Challenges/GuessingGameChallenge/README.md
@@ -3,7 +3,7 @@
 Write a program that picks a random integer from 1 to 100, and has players guess the number. The rules are:
 1. If a player's guess is less than 1 or greater than 100, say "OUT OF BOUNDS"
 2. On a player's first turn, if their guess is
-	- 	within-  10 of the number, return "WARM!"
+	- 	within 10 of the number, return "WARM!"
 	- 	further than 10 away from the number, return "COLD!"
 3. On all subsequent turns, if a guess is
 	- 	 closer to the number than the previous guess return "WARMER!"
