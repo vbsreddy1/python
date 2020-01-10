ad6dbc87c45a3c6d56d469797ed2a7c80780420c
vbsreddy <vintabharathsaireddy@gmail.com>
2020-01-10 15:49:20 +0530

    tictactoe done

--- Challenges/TicTacToeGame/TicTacToeGame.py
+++ Challenges/TicTacToeGame/TicTacToeGame.py
@@ -1,5 +1,23 @@
 
-board = [0,0,0,0,0,0,0,0,0]
+board = [' ']*9
+
+from os import system, name
+# define our clear function
+def screen_clear():
+    _ = system('clear')
+
+def display_board():
+    print('   |   |')
+    print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('   |   |')
 
 def checkBoard():
     if (board[0], board[1], board[2]) == ('X', 'X', 'X') or (board[3], board[4], board[5]) == ('X', 'X', 'X') or (board[6], board[7], board[8]) == ('X', 'X', 'X') or (board[0], board[3], board[6]) == ('X', 'X', 'X') or (board[1], board[4], board[7]) == ('X', 'X', 'X') or (board[2], board[5], board[8]) == ('X', 'X', 'X') or (board[0], board[4], board[8]) == ('X', 'X', 'X') or (board[2], board[4], board[6]) == ('X', 'X', 'X'):
@@ -8,15 +26,16 @@ def checkBoard():
     elif (board[0], board[1], board[2]) == ('O', 'O', 'O') or (board[3], board[4], board[5]) == ('O', 'O', 'O') or (board[6], board[7], board[8]) == ('O', 'O', 'O') or (board[0], board[3], board[6]) == ('O', 'O', 'O') or (board[1], board[4], board[7]) == ('O', 'O', 'O') or (board[2], board[5], board[8]) == ('O', 'O', 'O') or (board[0], board[4], board[8]) == ('O', 'O', 'O') or (board[2], board[4], board[6]) == ('O', 'O', 'O'):
         print('Player-2 wins')
         return 1
-    elif 0 not in board:
+    elif ' ' not in board:
         print('Draw')
         return 1
 
+screen_clear()
 while True:
     while(True):
         try:
-            board_position = int(input("Please enter a number for the board position choice: "))
-            if board[board_position-1] == 0:
+            board_position = int(input("Player-1: Please enter a number for the board position choice: "))
+            if board[board_position-1] == ' ':
                 board[board_position-1] = "X"
                 break
             else:
@@ -24,13 +43,15 @@ while True:
         except:
             print("Please enter a number for board position within 1-9")
 
+    screen_clear()
+    display_board()
     if checkBoard()==1:
         break
 
     while(True):
         try:
-            board_position = int(input("Please enter a number for the board position choice: "))
-            if board[board_position-1] == 0:
+            board_position = int(input("Player-2: Please enter a number for the board position choice: "))
+            if board[board_position-1] == ' ':
                 board[board_position-1] = "O"
                 break
             else:
@@ -38,5 +59,7 @@ while True:
         except:
             print("Please enter a number for board position within 1-9")
 
+    screen_clear()
+    display_board()
     if checkBoard()==1:
         break
