bddc35effee95b5fdfbe6f5d8752c8875a7bb99d
vbsreddy <vintabharathsaireddy@gmail.com>
2020-01-10 15:18:11 +0530

    tictactoe basic

---
+++ Challenges/TicTacToeGame/TicTacToeGame.py
@@ -0,0 +1,42 @@
+
+board = [0,0,0,0,0,0,0,0,0]
+
+def checkBoard():
+    if (board[0], board[1], board[2]) == ('X', 'X', 'X') or (board[3], board[4], board[5]) == ('X', 'X', 'X') or (board[6], board[7], board[8]) == ('X', 'X', 'X') or (board[0], board[3], board[6]) == ('X', 'X', 'X') or (board[1], board[4], board[7]) == ('X', 'X', 'X') or (board[2], board[5], board[8]) == ('X', 'X', 'X') or (board[0], board[4], board[8]) == ('X', 'X', 'X') or (board[2], board[4], board[6]) == ('X', 'X', 'X'):
+        print('Player-1 wins')
+        return 1
+    elif (board[0], board[1], board[2]) == ('O', 'O', 'O') or (board[3], board[4], board[5]) == ('O', 'O', 'O') or (board[6], board[7], board[8]) == ('O', 'O', 'O') or (board[0], board[3], board[6]) == ('O', 'O', 'O') or (board[1], board[4], board[7]) == ('O', 'O', 'O') or (board[2], board[5], board[8]) == ('O', 'O', 'O') or (board[0], board[4], board[8]) == ('O', 'O', 'O') or (board[2], board[4], board[6]) == ('O', 'O', 'O'):
+        print('Player-2 wins')
+        return 1
+    elif 0 not in board:
+        print('Draw')
+        return 1
+
+while True:
+    while(True):
+        try:
+            board_position = int(input("Please enter a number for the board position choice: "))
+            if board[board_position-1] == 0:
+                board[board_position-1] = "X"
+                break
+            else:
+                print("Kindly please check the board position you entered is already filled.")
+        except:
+            print("Please enter a number for board position within 1-9")
+
+    if checkBoard()==1:
+        break
+
+    while(True):
+        try:
+            board_position = int(input("Please enter a number for the board position choice: "))
+            if board[board_position-1] == 0:
+                board[board_position-1] = "O"
+                break
+            else:
+                print("Kindly please check the board position you entered is already filled.")
+        except:
+            print("Please enter a number for board position within 1-9")
+
+    if checkBoard()==1:
+        break
