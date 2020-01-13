073e9e4e5333b651ad9be27d8631381d667bf2cf
vbsreddy <vintabharathsaireddy@gmail.com>
2020-01-13 13:01:23 +0530

    push

--- Challenges/TicTacToeGame/TicTacToeGame.py
+++ Challenges/TicTacToeGame/TicTacToeGame.py
@@ -1,4 +1,6 @@
-
+player1count = 0
+player2count = 0
+totalcount = 0
 
 from os import system, name
 # define our clear function
@@ -27,20 +29,26 @@ def display_board(board):
 
 def checkBoard(board):
     if (board[0], board[1], board[2]) == ('X', 'X', 'X') or (board[3], board[4], board[5]) == ('X', 'X', 'X') or (board[6], board[7], board[8]) == ('X', 'X', 'X') or (board[0], board[3], board[6]) == ('X', 'X', 'X') or (board[1], board[4], board[7]) == ('X', 'X', 'X') or (board[2], board[5], board[8]) == ('X', 'X', 'X') or (board[0], board[4], board[8]) == ('X', 'X', 'X') or (board[2], board[4], board[6]) == ('X', 'X', 'X'):
-        print('Player-1 wins')
+        print(f'{player1} wins')
+        print(player1count)
+        player1count = player1count+1
+        totalcount = totalcount+1
         return 1
     elif (board[0], board[1], board[2]) == ('O', 'O', 'O') or (board[3], board[4], board[5]) == ('O', 'O', 'O') or (board[6], board[7], board[8]) == ('O', 'O', 'O') or (board[0], board[3], board[6]) == ('O', 'O', 'O') or (board[1], board[4], board[7]) == ('O', 'O', 'O') or (board[2], board[5], board[8]) == ('O', 'O', 'O') or (board[0], board[4], board[8]) == ('O', 'O', 'O') or (board[2], board[4], board[6]) == ('O', 'O', 'O'):
-        print('Player-2 wins')
+        print(f'{player2} wins')
+        player2count = player2count+1
+        totalcount = totalcount+1
         return 1
     elif ' ' not in board:
         print('Draw')
+        totalcount = totalcount+1
         return 1
 
-def game(playernum, board):
+def game(playername,playernum, board):
     mark = ' '
     while(True):
         try:
-            board_position = int(input(f"Player-{playernum}: Please enter a number for the board position choice: "))
+            board_position = int(input(f"{playername}: Please enter a number for the board position choice: "))
             if playernum == 1:
                 mark = "X"
             else:
@@ -54,20 +62,22 @@ def game(playernum, board):
             print("Please enter a number for board position within 1-9")
 
 
+screen_clear()
+player1 = input("Enter Player-1 Name:")
+player2 = input("Enter Player-2 Name:")
+print(f'{player1} symbol ******* X \n{player2} symbol ------- O')
 
 while True:
     board = [' ']*9
-    screen_clear()
-    print('Player-1 symbol ******* X \nPlayer-2 symbol ------- O')
     display_board(board)
     while True:
-        board = game(1, board)
+        board = game(player1, 1, board)
         screen_clear()
         display_board(board)
         if checkBoard(board)==1:
             break
 
-        board = game(2, board)
+        board = game(player2, 2, board)
 
         screen_clear()
         display_board(board)
@@ -78,3 +88,10 @@ while True:
     replay = replay.upper()
     if replay != 'YES' and replay != 'Y':
         break
+    screen_clear()
+
+drawmatches = totalcount - (player1count+player2count)
+print(f"Total Matches Played:{totalcount}")
+print(f"{player1} won: {player1count}")
+print(f"{player2} won: {player2count}")
+print(f"Draw matches: {drawmatches}")
