6aae505bd8691724c6e9899fae0f11c92a708a8d
vbsreddy <vintabharathsaireddy@gmail.com>
2020-02-25 00:10:48 +0530

    my changes

--- Challenges/RomanToInt/RomanToInt.py
+++ Challenges/RomanToInt/RomanToInt.py
@@ -1,16 +1,16 @@
-def romanToInt(romans:dict, s: str) -> int:
-    #romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+def romanToInt(romans: dict, s: str) -> int:
+    # romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     out = 0
     i = 0
     if len(s) >= 2:
-        while i < (len(s)-2):
-            if romans[s[i]] < romans[s[i+1]]:
-                out = out + romans[s[i+1]] - romans[s[i]]
-                i = i+2
+        while i < (len(s) - 2):
+            if romans[s[i]] < romans[s[i + 1]]:
+                out = out + romans[s[i + 1]] - romans[s[i]]
+                i = i + 2
                 continue
             out = out + romans[s[i]]
-            i = i+1
-        if len(s)-i == 1:
+            i = i + 1
+        if len(s) - i == 1:
             out = out + romans[s[-1]]
         else:
             if romans[s[-2]] < romans[s[-1]]:
